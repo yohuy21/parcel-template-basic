b33d621ca11728f4b6c52949f9c08a8d02c41a78
yohuy21 <yohuy21@gmail.com>
2021-05-22 12:37:23 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+/* const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+} */
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
